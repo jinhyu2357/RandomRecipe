e24d807192d333eb73cb1fb2483274674547f965
jinhyu2357 <jinhyu2357@gmail.com>
2026-03-08 00:40:03 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/jinhhyu/randomRecipe/RandomRecipe.kt
@@ -0,0 +1,14 @@
+package org.example.jinhhyu.randomRecipe
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RandomRecipe : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
